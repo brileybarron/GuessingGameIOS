0db165ff4e4f8cdcaf868575d6c8d5c9dc58d6e0
Briley Barron <brileybarron@MacBook-Pro-101.local>
2018-10-09 14:53:15 -0500

    Got some of the logic worked out.  Made it to where a random number is chosen for easy medium and hard.

--- GuessingGame/GuessingScreenVC.swift
+++ GuessingGame/GuessingScreenVC.swift
@@ -9,9 +9,29 @@
 import UIKit
 
 class GuessingScreenVC: UIViewController {
-
-    @IBOutlet weak var rand: UILabel!
-    override func viewDidLoad() {
+    var viewController = ViewController()
+    @IBOutlet weak var instructionLabel: UILabel!
+    @IBOutlet weak var guessingField: UITextField!
+    @IBOutlet weak var guessesRemaining: UILabel!
+    @IBOutlet weak var highOrLow: UILabel!
+    var guesses = 5
+    @IBAction func guessButtonTapped(_ sender: Any) {
+        if let intGuess = Int(guessingField.text!) {
+            if intGuess > ViewController.randomNumber {
+                highOrLow.text = "Too High"
+            } else if intGuess < ViewController.randomNumber {
+                highOrLow.text = "Too Low"
+            }
+            guesses -= 1
+            guessesRemaining.text = "\(guesses) guesses remaining"
+        } else {
+            highOrLow.text = "Please use a number"
+        }
+    }
+    
+    
+    
+        override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
--- GuessingGame/ViewController.swift
+++ GuessingGame/ViewController.swift
@@ -9,19 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBAction func chooseANumberZeroToOneHundredButtonTapped(_ sender: Any) {
-        let randomNumber = Int.random(in: 0...100)
-        print("""
-You will have five guesses to figure out what the random number is.  There will be a hint after each guess that should lead you closer to the answer.  Good Luck!!
-
-
-Please choose a number from 0-100
-""")
-        
+    static var randomNumber : Int = 77
+    @IBAction func oneToOneHundredButtonTapped(_ sender: Any) {
+        ViewController.randomNumber = Int.random(in: 0...100)
     }
     
+    @IBAction func oneToFiveHundredButtonTapped(_ sender: Any) {
+        ViewController.randomNumber = Int.random(in: 0...500)
+    }
     
+    @IBAction func oneToOneThousandButtonTapped(_ sender: Any) {
+        ViewController.randomNumber = Int.random(in: 0...1000)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
